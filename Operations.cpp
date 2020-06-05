836870caa5cca3e2dd221eeafb8002d75c626c40
faraice <faraicerehab@gmail.com>
2020-06-05 12:07:19 +0200

    Update Operations.cpp

--- Operations.cpp
+++ Operations.cpp
@@ -17,3 +17,15 @@ int soustraction(int a, int b)
 	
 	return resultat;
 }
+int multiplication(int a, int b)
+{
+	int res;
+	res = a*b;
+	return res;
+}
+int division(int a, int b)
+{
+	int res;
+	res = a/b;
+	return res;
+}
